5bb67a34364d9ad359bc3a3da2134929f7413158
Mark Munyaka <markmunyaka@gmail.com>
2025-12-02 14:43:32 +0000

    Created Notes list page

---
+++ frontend/pages/notes/index.vue
@@ -0,0 +1,11 @@
+<script setup>
+    const { data: notes } = await useFetch('http://localhost:1337/api/notes')
+</script>
+
+<template>
+    <h1>Notes App</h1>
+    <h2>Notes List</h2>
+    <ul>
+        <li v-for="note in notes.data">{{ note.title }}</li>
+    </ul>
+</template>
