5ff42f525f787d011a172169d66ec8d3afc880c2
Mark Munyaka <markmunyaka@gmail.com>
2025-12-03 15:49:08 +0000

    Set up "Create Note" flow
    Create `create.vue` file

---
+++ frontend/pages/notes/create.vue
@@ -0,0 +1,17 @@
+<template>
+    <h1>Notes App</h1>
+    <h2>Create a New Note</h2>
+
+    <form action="/api/notes/create" method="POST">
+        <div>
+            <label for="title">Title:</label>
+            <input type="text" name="title" id="title" required />
+        </div>
+        <div>
+            <label for="content">Content:</label>
+            <textarea name="content" id="content" required></textarea>
+        </div>
+
+        <button type="submit">Create Note</button>
+    </form>
+</template>
