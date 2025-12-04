324d7dbf903b8a4d8dd321ee6178446fc2106006
Mark Munyaka <markmunyaka@gmail.com>
2025-12-04 11:57:41 +0000

    Create `deleted.vue` file for confirmation

---
+++ frontend/pages/notes/deleted.vue
@@ -0,0 +1,7 @@
+<template>
+    <h1>Notes App</h1>
+    <h2>Note deleted successfully</h2>
+    <p>Your note has been deleted successfully.</p>
+    <br/>
+    <NuxtLink to="/notes">Back to All Notes</NuxtLink>
+</template>
