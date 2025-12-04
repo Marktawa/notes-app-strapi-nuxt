83a95a9855605cd24dccdbed412cf6177cfabe45
Mark Munyaka <markmunyaka@gmail.com>
2025-12-04 05:36:34 +0000

    Create `edited.vue` file for confirmation

---
+++ frontend/pages/notes/edited.vue
@@ -0,0 +1,16 @@
+<script setup>
+    const route = useRoute()
+    const noteId = route.query.id
+</script>
+
+<template>
+    <h1>Notes App</h1>
+    <h2>Note edited successfully</h2>
+    <p>Your note has been updated.</p>
+
+    <NuxtLink :to="`/notes/${noteId}`">
+        <button>View Edited Note</button>
+    </NuxtLink>
+    <br>
+    <NuxtLink to="/notes">Back to All Notes</NuxtLink>
+</template>
