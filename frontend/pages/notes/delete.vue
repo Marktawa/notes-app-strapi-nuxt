957fee548f9a16a34cbde96e13d6c84893a99886
Mark Munyaka <markmunyaka@gmail.com>
2025-12-04 11:56:59 +0000

    Create `delete.vue` file

---
+++ frontend/pages/notes/delete.vue
@@ -0,0 +1,23 @@
+<script setup>
+    const route = useRoute()
+    const noteId = route.query.id
+    const { data: note } = await useFetch(`http://localhost:1337/api/notes/${noteId}`)
+</script>
+
+<template>
+    <h1>Notes App</h1>
+    <h2>Delete Note</h2>
+
+    <h3>{{ note.data.title }}</h3>
+    <p>{{  note.data.content }}</p>
+
+    <p>Are you sure you want to delete this note?</p>
+    <form action="/api/notes/delete" method="POST">
+        <input type="hidden" name="documentId" :value="noteId" />
+        <button type="submit">Yes</button>
+    </form>
+
+    <NuxtLink :to="`/notes/${noteId}`">
+        <button>No</button>
+    </NuxtLink>
+</template>
